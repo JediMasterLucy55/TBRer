261a02954fa0666cc53d6cc18fef98213f13cc01
JediMasterLucy <lucia.hans@icloud.com>
2026-05-28 10:15:15 -0500

    yay exams are done

--- app/src/Book.java
+++ app/src/Book.java
@@ -1,4 +1,4 @@
-package src.main;
+package app.src.main;
 
 public class Book {
     
---
+++ app/src/Chooser.java
@@ -0,0 +1,10 @@
+package app.src.main;
+import com.google.gson.Gson;  
+
+public class Chooser {
+
+    public Chooser() {
+        super();
+    }
+
+}

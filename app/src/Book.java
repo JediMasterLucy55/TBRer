94f70cca32a9df05faa7ba063ffe31b2149115b7
JediMasterLucy <lucia.hans@icloud.com>
2026-05-28 10:05:46 -0500

    stuff

---
+++ app/src/Book.java
@@ -0,0 +1,24 @@
+package src.main;
+
+public class Book {
+    
+    String title;
+    int fellowship;
+    int emotionalDamage;
+    int brainPower;
+    int romance;
+    int coverArt;
+    int realism;
+
+    public Book(String title, int fellowship, int emotionalDamage, int brainPower, int romance, int coverArt, int realism) {
+        super();
+        this.title = title;
+        this.fellowship = fellowship;
+        this.emotionalDamage = emotionalDamage;
+        this.brainPower = brainPower;
+        this.romance = romance;
+        this.coverArt = coverArt;
+        this.realism = realism;
+    }
+
+}
---
+++ app/src/main/java/org/example/Book.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Book {
+    
+    String title;
+    int fellowship;
+    int emotionalDamage;
+    int brainPower;
+    int romance;
+    int coverArt;
+    int realism;
+
+    public Book(String title, int fellowship, int emotionalDamage, int brainPower, int romance, int coverArt, int realism) {
+        this.title = title;
+        this.fellowship = fellowship;
+        this.emotionalDamage = emotionalDamage;
+        this.brainPower = brainPower;
+        this.romance = romance;
+        this.coverArt = coverArt;
+        this.realism = realism;
+    }
+
+}

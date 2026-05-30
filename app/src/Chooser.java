a23cc90b2e22c8050a2df65f1861d7630cc3a185
JediMasterLucy <lucia.hans@icloud.com>
2026-05-29 19:21:38 -0500

    shembulock

--- app/src/Chooser.java
+++ app/src/Chooser.java
@@ -1,7 +1,19 @@
-import com.google.gson.Gson;  
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.io.FileReader;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class Chooser {
 
-    
+    FileReader reader = new FileReader("books.json");
+
+    Gson gson = new Gson();
+
+    Type type = new TypeToken<ArrayList<Book>>(){}.getType();
+
+    public ArrayList<Book> books = gson.fromJson(reader, type);
+
+    reader.close();
 
 }
--- app/src/Main.java
+++ app/src/Main.java
@@ -1,5 +1,11 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Hello! I will help you choose your book. \n Everything is on a scale of 1-10");
+        System.out.println("How relatable would you like the characters in the book to be?");
+        int fellowship = scanner.ge
     }
 }
